dc9eedece769ea7c05a49e3d3bffdf885983aba8
kazuki-310 <kazu.0310123@gmail.com>
2025-01-31 23:55:59 +0900

    add db 周りの処理

--- app/_components/dashboard/sidenav.tsx
+++ app/_components/dashboard/sidenav.tsx
@@ -11,9 +11,10 @@ export default function SideNav() {
 					<AcmeLogo />
 				</div>
 			</Link>
+
 			<div className='flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2'>
 				<NavLinks />
-				<div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block'>test</div>
+				<div className='hidden h-auto w-full grow rounded-md bg-gray-50 md:block' />
 				<form>
 					<button
 						type='button'
