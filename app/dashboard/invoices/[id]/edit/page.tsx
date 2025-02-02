bd35c1ef7b2987cbefc17a399c9ae5c14a77e822
kazuki-310 <kazu.0310123@gmail.com>
2025-02-02 17:25:04 +0900

    add: 請求書のエラーハンドリングと404ページの追加

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,12 +1,17 @@
 import { Breadcrumbs } from '@/app/_components/invoices/breadcrumbs';
 import EditInvoiceForm from '@/app/_components/invoices/edit-form';
 import { fetchInvoiceById } from '@/app/_lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
 	const params = await props.params;
 	const id = params.id;
 
 	const invoice = await fetchInvoiceById(id);
+
+	if (!invoice) {
+		notFound();
+	}
 
 	return (
 		<main>
